1b058075b30b393fa48039519a69452f5c9f2661
Muhammed Arshaq <arshaq31@gmail.com>
2025-08-04 11:35:21 +0530

    add: create node usecase

--- backend/src/application/use-cases/CreateNodeTree.ts
+++ backend/src/application/use-cases/CreateNodeTree.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { INodeTreeRepository } from "../../domain/interface/nodetree.repository";
 import { CustomError } from "../../interface/middlewares/error.middleware";
 import { HttpStatus } from "../../utils/http.status";
@@ -6,8 +7,27 @@ export class CreateNodeTree {
   constructor(private nodeTreeRepository: INodeTreeRepository) {}
 
   async execute(name: string, parentId?: string | null) {
-    if (!name) {
-      throw new CustomError("Title is required", HttpStatus.BAD_REQUEST);
+    if (!name || name.trim().length === 0) {
+      throw new CustomError("Node name is required", HttpStatus.BAD_REQUEST);
+    }
+
+    if (name.trim().length > 100) {
+      throw new CustomError(
+        "Node name must be less than 100 characters",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    if (parentId) {
+      if (!mongoose.Types.ObjectId.isValid(parentId)) {
+        throw new CustomError("Invalid parent ID", HttpStatus.BAD_REQUEST);
+      }
+
+      const parent = await this.nodeTreeRepository.getNodeById(parentId);
+
+      if (!parent) {
+        throw new CustomError("Parent node not found", HttpStatus.NOT_FOUND);
+      }
     }
 
     const isExisting = await this.nodeTreeRepository.findNodeByName(name);
@@ -16,6 +36,13 @@ export class CreateNodeTree {
       throw new CustomError("Name already exists", HttpStatus.BAD_REQUEST);
     }
 
-    await this.nodeTreeRepository.createNode(name, parentId);
+    const savedNode = await this.nodeTreeRepository.createNode(name, parentId);
+
+    if (parentId) {
+      await this.nodeTreeRepository.findByParentIdAndUpdate(
+        parentId,
+        savedNode._id
+      );
+    }
   }
 }
